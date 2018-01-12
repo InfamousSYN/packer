8d10f9f439ac5dc46ccb3b36c6ab1f951fb010e2
infamoussyn <ainfamoussyn@gmail.com>
2018-01-12 00:42:35 -0500

    Fixing unset variable issue

--- scripts/common/linux/apt.sh
+++ scripts/common/linux/apt.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -eux
 
-if [[ $UPDATE  =~ true || $UPDATE =~ 1 || $UPDATE =~ yes ]]; then
-	export DEBIAN_FRONTEND=noninteractive
-	
-	echo "==> Updating list of repositories"
-    # apt-get update does not actually perform updates, it just downloads and indexes the list of packages
-    apt-get -y update
+export DEBIAN_FRONTEND=noninteractive
 
-    echo "==> Performing dist-upgrade (all packages and kernel)"
-    apt-get -y dist-upgrade --force-yes
-fi
+echo "==> Updating list of repositories"
+# apt-get update does not actually perform updates, it just downloads and indexes the list of packages
+apt-get -y update
+
+echo "==> Performing dist-upgrade (all packages and kernel)"
+apt-get -y dist-upgrade --force-yes
