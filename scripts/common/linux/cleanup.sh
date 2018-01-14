c7039af736aa8e8da5a8cebbc74050a577b05cd0
infamoussyn <ainfamoussyn@gmail.com>
2018-01-15 06:17:47 +1100

    pushing changes to cleanup script.sh

--- scripts/common/linux/cleanup.sh
+++ scripts/common/linux/cleanup.sh
@@ -1,13 +1,35 @@
-#!/bin/sh -eux
+#!/bin/bash -eux
 
-# Remove apt sources
-apt -y autoremove --purge
-apt -y clean
-apt -y autoclean
+# Cleanup apt cache
+apt-get -y autoremove --purge
+apt-get -y clean
+apt-get -y autoclean
 
-# Clear user's bash history
-history -c
+DISK_USAGE_BEFORE_CLEANUP=$(df -h)
 
-# Zero out hard drive for improved storage
-dd if=/dev/zero of=/EMPTY bs=1M
+# Remove Bash history
+unset HISTFILE
+rm -f /root/.bash_history
+rm -f /home/vagrant/.bash_history
+
+# Whiteout root
+count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+let count--
+dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
+rm /tmp/whitespace
+
+# Zero out the free space to save space in the final image
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
 rm -f /EMPTY
+
+# Make sure we wait until all the data is written to disk, otherwise
+# Packer might quite too early
+sync
+
+echo "==> Disk usage before cleanup"
+echo ${DISK_USAGE_BEFORE_CLEANUP}
+
+echo "==> Disk usage after cleanup"
+df -h
+
+sync;
